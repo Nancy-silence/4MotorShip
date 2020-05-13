7bf03ff32890e24ccf9f630e979b5d77ab4337e4
Nancy <970434796@qq.com>
2020-05-13 17:59:07 +0800

    move_point time related; move_point add v commen interleave

--- move_point.py
+++ move_point.py
@@ -5,36 +5,51 @@ import numpy as np
 import math
 
 class MovePoint(object):
-    def __init__(self, move_path='linear'):
-        self.point = np.array([0.0, 0.0, 0.0])
-        self.impl = getattr(self, move_path)
+    def __init__(self, target_trajectory='linear'):
+        self.position = np.array([0.0, 0.0, 0.0])
+        self.velocity = np.array([0.0, 0.0, 0.0])
+        self.t = 0.0
+        self.impl = getattr(self, target_trajectory)
 
     def reset(self):
-        self.point = np.array([0.0, 0.0, 0.0])
+        self.position = np.array([0.0, 0.0, 0.0])
+        self.velocity = np.array([0.0, 0.0, 0.0])
+        self.t = 0.0
 
-    @property
-    def position(self):
-        return self.point
+    # @property
+    # def position(self):
+    #     return self.position
 
     def next_point(self, interval):
-        return self.impl(interval)
+        self.t += interval
+        return self.impl(self.t)
 
-    def linear(self, interval):
-        x, y, theta = self.point
-        x += interval / 5
-        y = x
-        theta = math.pi / 4
-        self.point = np.array([x, y, theta])
-        return self.point
+    def linear(self, t):
+        x = t / 5
+        y = t / 5
+        vx = 0.2
+        vy = 0.2
+        theta = math.atan2(vy, vx)
+        u = math.sqrt(vx**2 + vy **2)
+        v = 0.0
+        r = 0.0
+        self.position = np.array([x, y, theta])
+        self.velocity = np.array([u, v, r])
+        return self.position, self.velocity
 
-    def func_sin(self, interval):
-        x, y, theta = self.point
-        x += interval / 5
-        y = -3 * np.cos(x) + 3
-        theta = math.atan(3 * np.sin(x))
-        self.point = np.array([x, y, theta])
-        return self.point
+    def func_sin(self, t):
+        x = t / 5
+        y = -3 * np.cos(t/5) + 3
+        vx = 0.2
+        vy = 0.6 * math.sin(t/5)
+        theta = math.atan2(vy, vx)
+        u = math.sqrt(vx**2 + vy **2)
+        v = 0.0
+        r = (0.6*math.cos(t/5)) / (1 + 9*np.power(math.sin(t/5),2))
+        self.position = np.array([x, y, theta])
+        self.velocity = np.array([u, v, r])
+        return self.position , self.velocity
 
-    def random(self, interval):
-        self.point = np.random.randint(0, 100, 2)
-        return self.point
+    def random(self, t):
+        self.position = np.random.randint(0, 100, 2)
+        return self.position, self.velocity
